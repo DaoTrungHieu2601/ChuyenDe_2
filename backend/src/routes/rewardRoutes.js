3e71c6ec7d13ca80a5eaff409358b476cdc16983
DaoTrungHieu2601 <hieutoki26012004@gmail.com>
2026-03-31 18:54:04 +0700

    Tuần 1: tài liệu tuần 1

--- backend/src/routes/rewardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getUserBadges, getLearningPath } = require('../controllers/rewardController');
-const auth = require('../middleware/auth');
-
-router.get('/badges', auth, getUserBadges);
-router.get('/learning-path', auth, getLearningPath);
-
-module.exports = router;
--- backend/src/routes/topicRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllTopics, getTopicById } = require('../controllers/topicController');
-const auth = require('../middleware/auth');
-
-router.get('/', auth, getAllTopics);
-router.get('/:id', auth, getTopicById);
-
-module.exports = router;
